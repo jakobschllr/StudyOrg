6038ccf09ecd2a33cbefd0167a049e84c5a14527
Jakob Schiller <jakobschiller23@gmail.com>
2025-01-23 19:05:36 +0100

    still trying to fix bug

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ import express  from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename);
+
 dotenv.config()
 const app = express()
-app.use(express.static('../dist')) // frontend build
+app.use(express.static(resolve(__dirname, '../dist'))) // frontend build
 app.use(express.json()) // middleware for parsing incoming requests
 app.use(cors())
 
