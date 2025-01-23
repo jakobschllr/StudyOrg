8eed1b90bb9bd5067765b4877190d28be3155096
Jakob Schiller <jakobschiller23@gmail.com>
2025-01-23 19:18:55 +0100

    still fixing bugs

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import express  from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename);
+import path from 'path'
 
 dotenv.config()
 const app = express()
-app.use(express.static(resolve(__dirname, '../dist'))) // frontend build
+app.use(
+    express.static(path.resolve('dist'), {
+      setHeaders: (res, filePath) => {
+        if (filePath.endsWith('.css')) {
+          res.setHeader('Content-Type', 'text/css')
+        }
+      },
+    })
+  )
 app.use(express.json()) // middleware for parsing incoming requests
 app.use(cors())
 
