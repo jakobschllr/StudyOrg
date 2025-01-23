08a1043db38f8bdbc0f1d99b35ab8cfc07e40633
Jakob Schiller <jakobschiller23@gmail.com>
2025-01-23 19:59:46 +0100

    Fixed bug

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
 import express  from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import path from 'path'
 
 dotenv.config()
 const app = express()
-app.use(
-    express.static(path.resolve('dist'), {
-      setHeaders: (res, filePath) => {
-        if (filePath.endsWith('.css')) {
-          res.setHeader('Content-Type', 'text/css')
-        }
-      },
-    })
-  )
-app.use(express.json()) // middleware for parsing incoming requests
+app.use(express.static('../dist'))
+app.use(express.json())
 app.use(cors())
 
 export default app
